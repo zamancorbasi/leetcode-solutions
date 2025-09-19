e4cac8adf6a7478f723160e7bcb292442435889a
ilayda ras tiryaki <ilaydatiyaki25@gmail.com>
2025-09-19 21:01:02 +0300

    two sum

---
+++ easy/leetcode-twoSum/leetcode-twoSum/leetcode-twoSum.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int length = nums.size();
+        vector<int> results;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    results.push_back(i);
+                    results.push_back(j);
+                    return results;
+                }
+            }
+        }
+
+        return {}; // boş vektör, çözüm yoksa
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = { 2, 7, 11, 15 };
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (result.empty()) {
+        cout << "Çözüm yok" << endl;
+    }
+    else {
+        cout << "Indexler: ";
+        for (int idx : result) {
+            cout << idx << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
