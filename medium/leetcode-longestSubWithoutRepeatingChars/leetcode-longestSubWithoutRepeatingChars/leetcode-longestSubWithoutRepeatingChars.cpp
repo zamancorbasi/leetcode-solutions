f40c2c4e64f3a07b719b1dbda1523cc4dfa85096
ilayda ras tiryaki <ilaydatiyaki25@gmail.com>
2025-09-22 18:12:39 +0300

    longest substring without repeating characters (problem.3 in medium)

---
+++ medium/leetcode-longestSubWithoutRepeatingChars/leetcode-longestSubWithoutRepeatingChars/leetcode-longestSubWithoutRepeatingChars.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        string sub = "";
+        sub[0] = s[0];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 1; j < s.length(); j++) {
+                sub += s[j];
+                if (s[i] == s[j]) {
+                    break;
+                }
+            }
+            break;
+        }
+
+        cout << sub;
+
+        return sub.length();
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<string> testCases = {
+        "abcabcbb",
+        "bbbbb",
+        "pwwkew",
+        "",
+        "au",
+        "dvdf"
+    };
+
+    for (auto& tc : testCases) {
+        int result = sol.lengthOfLongestSubstring(tc);
+        cout << "Input: \"" << tc << "\" -> Output: " << result << endl;
+    }
+
+    return 0;
+
+
+    return 0;
+}
